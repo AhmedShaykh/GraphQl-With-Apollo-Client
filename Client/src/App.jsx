287ea323f553861f5af150a93e30b0e76f80ecca
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2022-06-22 01:40:48 +0500

    It's Time To Push !

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
+import Dev from './Dev';
 
 function App() {
   return (
     <div className='App-header'>
       <h2>My First Apollo App 🚀</h2>
+      <Dev />
     </div>
   );
 }
--- Client/src/Dev.jsx
+++ Client/src/Dev.jsx
@@ -1,10 +1,41 @@
 import React from "react";
+import { useQuery, gql } from "@apollo/client";
+
+const GET_API = gql`
+  query GetAllAPIs {
+    Developers {
+        name,
+        age,
+        email,
+        id
+      }
+  }
+`;
 
 function Dev() {
-  return (
-    <div>
-    </div>
-  );
+    const { loading, error, data } = useQuery(GET_API);
+
+    if (loading) return <h3>Loading...</h3>;
+    if (error) return <h3>Error :(</h3>;
+
+    const { developers } = data;
+
+    return (
+        <div>
+            <h2>Developers List :</h2>
+            <table>
+                <h3>
+                    {
+                        developers.map(dev => {
+                            return(<tr>
+                                <td>{dev.name}</td>
+                            </tr>)
+                        })
+                    }
+                </h3>
+            </table>
+        </div>
+    );
 }
 
 export default Dev;
